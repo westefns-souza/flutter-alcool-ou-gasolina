b9abdb0340327a887da1f59877e2b027631645b7
Westefns Damião de Souza Silva <l4304@liga.org.br>
2021-02-04 15:02:21 -0300

    Aula Loading Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_alcool_ou_gasolina/widgets/input.widget.dart';
+import 'package:flutter_alcool_ou_gasolina/widgets/loading-button.widget.dart';
 import 'package:flutter_alcool_ou_gasolina/widgets/logo.widget.dart';
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 
@@ -55,25 +56,11 @@ class HomePage extends StatelessWidget {
                 SizedBox(
                   height: 20,
                 ),
-                Container(
-                  margin: EdgeInsets.all(30),
-                  height: 60,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.white.withOpacity(0.8),
-                    borderRadius: BorderRadius.circular(60),
-                  ),
-                  child: FlatButton(
-                    child: Text(
-                      "CALCULAR NOVAMENTE",
-                      style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                        fontSize: 25,
-                        fontFamily: "Big Shoulders Display",
-                      ),
-                    ),
-                    onPressed: () {},
-                  ),
+                LoadingButton(
+                  busy: false,
+                  func: () {},
+                  text: "CALCULAR NOVAMENTE",
+                  invert: true,
                 ),
               ],
             ),
@@ -86,24 +73,11 @@ class HomePage extends StatelessWidget {
             label: "Álcool",
             ctrl: _alcCtrl,
           ),
-          Container(
-            margin: EdgeInsets.all(30),
-            height: 60,
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(60),
-            ),
-            child: FlatButton(
-              child: Text(
-                "CALCULAR",
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 25,
-                  fontFamily: "Big Shoulders Display",
-                ),
-              ),
-              onPressed: () {},
-            ),
+          LoadingButton(
+            busy: true,
+            func: () {},
+            text: "CALCULAR",
+            invert: false,
           ),
         ],
       ),
