ce5144de4f641823cd167cfaf50d5d83d8b14a32
Westefns Damião de Souza Silva <l4304@liga.org.br>
2021-02-04 14:21:02 -0300

    Aula Botão Calcular

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,25 @@ class HomePage extends StatelessWidget {
             label: "Álcool",
             ctrl: _alcCtrl,
           ),
+          Container(
+            margin: EdgeInsets.all(30),
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(60),
+            ),
+            child: FlatButton(
+              child: Text(
+                "CALCULAR",
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 25,
+                  fontFamily: "Big Shoulders Display",
+                ),
+              ),
+              onPressed: () {},
+            ),
+          ),
         ],
       ),
     );
