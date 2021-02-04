f44f3de7aa9f3e940f498b1bee80d30b7a5c87fc
Westefns Damião de Souza Silva <l4304@liga.org.br>
2021-02-04 14:48:32 -0300

    Aula Exibindo o Resultado

--- lib/main.dart
+++ lib/main.dart
@@ -32,6 +32,52 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: [
           Logo(),
+          Container(
+            margin: EdgeInsets.all(30),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  "Compensa utilizar álcool",
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 40,
+                    fontFamily: "Big Shoulders Display",
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  margin: EdgeInsets.all(30),
+                  height: 60,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.8),
+                    borderRadius: BorderRadius.circular(60),
+                  ),
+                  child: FlatButton(
+                    child: Text(
+                      "CALCULAR NOVAMENTE",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 25,
+                        fontFamily: "Big Shoulders Display",
+                      ),
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+          ),
           Input(
             label: "Gasolina",
             ctrl: _gasCtrl,
